53c5757af4c7626ac3b9515d3db00f25967212ab
doantaa <doantaaloycius@gmail.com>
2023-11-28 18:04:33 +0700

    Create Category Item, Course Item layout and Home Fragment Layout

--- app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
@@ -1,12 +1,10 @@
-package com.cious.learnhub
+package com.cious.learnhub.ui.main
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.cious.learnhub.R
 import com.cious.learnhub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
