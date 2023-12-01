376cde99e3e83602e69c242d8528a78944581f74
noorizamodiss <noorizamodistira@gmail.com>
2023-12-01 22:08:49 +0700

    Finishing Splashscreen

---
+++ app/src/main/java/com/cious/learnhub/Splashscreen/OnBoardingSecondFragment.kt
@@ -0,0 +1,29 @@
+package com.cious.learnhub.Splashscreen
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+
+class OnBoardingSecondFragment : Fragment() {
+    private lateinit var btNext: ImageButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_on_boarding2, container, false)
+        btNext = view.findViewById(R.id.btNext)
+
+        btNext.setOnClickListener {
+            if (activity is OnboardingNavigation) {
+                (activity as OnboardingNavigation).navigateToNextFragment(2)
+            }
+        }
+
+        return view
+    }
+}
