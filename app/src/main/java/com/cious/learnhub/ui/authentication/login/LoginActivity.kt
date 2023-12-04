589a7cf3debafbd2a0706d201dd09ccc22cc9f0e
Muhammad Izzudin <mohammadzzudin@gmail.com>
2023-12-05 00:29:42 +0700

    UI payment done

--- app/src/main/java/com/cious/learnhub/ui/authentication/login/LoginActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/authentication/login/LoginActivity.kt
@@ -40,7 +40,6 @@ class LoginActivity : AppCompatActivity() {
             Intent(this, ResetPasswordActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             }
-
         )
     }
 
---
+++ app/src/main/java/com/cious/learnhub/ui/payment/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.cious.learnhub.ui.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cious.learnhub.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
