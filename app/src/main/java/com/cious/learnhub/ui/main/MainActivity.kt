7fdfecb38bae7a344be29d43a8119ad88087b27c
Abdurrohman11 <erna40408@gmail.com>
2023-12-19 20:26:39 +0700

    splash screen titik2nya lupa di ganti

--- app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.cious.learnhub.ui.main
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,16 +17,42 @@ import com.cious.learnhub.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupBottomNav()
+//        setupBottomNav()
+
+        val navHost = binding.navHostFragmentActivityMain.getFragment<Fragment>() as NavHostFragment
+
+        navController = navHost.navController
+        binding.navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.navigation_home -> hideBottomNav(false)
+                R.id.navigation_notifications -> hideBottomNav(false)
+                R.id.navigation_my_class -> hideBottomNav(false)
+                R.id.navigation_course -> hideBottomNav(false)
+                R.id.navigation_profile -> hideBottomNav(false)
+                else -> hideBottomNav(true)
+
+            }
+        }
     }
 
-    private fun setupBottomNav() {
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        binding.navView.setupWithNavController(navController)
+    private fun hideBottomNav(hide: Boolean) {
+        if (hide){
+            binding.navView.visibility = View.GONE
+        }else{
+            binding.navView.visibility = View.VISIBLE
+        }
+
     }
+
+//    private fun setupBottomNav() {
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        binding.navView.setupWithNavController(navController)
+//    }
 }
