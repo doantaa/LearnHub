9a475a273ff32cbfbe96284558ca506d27023560
Abdurrohman11 <erna40408@gmail.com>
2023-12-19 20:26:39 +0700

    splash screen titik2nya lupa di ganti

--- app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.cious.learnhub.ui.main
 
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cious.learnhub.R
 import com.cious.learnhub.databinding.ActivityMainBinding
@@ -11,13 +18,42 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel : MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setupBottomNav()
+        setIntropageNav()
+    }
+
+    private fun setIntropageNav() {
+        val navController: NavController
+        val navHost = binding.navHostFragmentActivityMain.getFragment<Fragment>() as NavHostFragment
+
+        navController = navHost.navController
+        binding.navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.navigation_home -> hideBottomNav(false)
+                R.id.navigation_notifications -> hideBottomNav(false)
+                R.id.navigation_my_class -> hideBottomNav(false)
+                R.id.navigation_course -> hideBottomNav(false)
+                R.id.navigation_profile -> hideBottomNav(false)
+                else -> hideBottomNav(true)
+
+            }
+        }
+    }
+
+    private fun hideBottomNav(hide: Boolean) {
+        if (hide){
+            binding.navView.visibility = View.GONE
+        }else{
+            binding.navView.visibility = View.VISIBLE
+        }
+
+    }
     }
 
     private fun setupBottomNav() {
---
+++ app/src/main/java/com/cious/learnhub/ui/onboarding/screen/OnBoardingScreenSecond.kt
@@ -0,0 +1,32 @@
+package com.cious.learnhub.ui.onboarding.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.cious.learnhub.R
+import com.google.android.material.button.MaterialButton
+
+
+class OnBoardingScreenSecond : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_on_boarding_screen_second, container, false)
+
+        val viewPager =  activity?.findViewById<ViewPager2>(R.id.viewPager)
+
+        view.findViewById<MaterialButton>(R.id.btn_next2).setOnClickListener {
+            viewPager?.currentItem = 2
+        }
+
+        return view
+    }
+
+
+}
