2b69306ed18881c35022ebb2db0c4b806dfe2b28
Muhammad Izzudin <mohammadzzudin@gmail.com>
2023-12-20 10:40:07 +0700

    Bottom sheet registration success done.

--- app/src/main/java/com/cious/learnhub/ui/authentication/otp/OtpActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/authentication/otp/OtpActivity.kt
@@ -2,18 +2,27 @@ package com.cious.learnhub.ui.authentication.otp
 
 import android.content.Context
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
 import androidx.core.view.isVisible
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.cious.learnhub.R
+import com.cious.learnhub.data.network.api.datasource.AuthDataSourceImpl
+import com.cious.learnhub.data.network.api.service.AuthenticationService
+import com.cious.learnhub.data.repository.AuthRepositoryImpl
 import com.cious.learnhub.databinding.ActivityOtpBinding
+import com.cious.learnhub.databinding.SheetRegistrationSuccessBinding
 import com.cious.learnhub.model.AuthenticationData
 import com.cious.learnhub.model.RegisterData
 import com.cious.learnhub.ui.authentication.register.RegisterActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.cious.learnhub.ui.main.MainActivity
+import com.cious.learnhub.utils.GenericViewModelFactory
 import com.cious.learnhub.utils.SessionManager
 import com.cious.learnhub.utils.proceedWhen
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.koin.core.parameter.parametersOf
 
 class OtpActivity : AppCompatActivity() {
@@ -22,10 +31,15 @@ class OtpActivity : AppCompatActivity() {
         ActivityOtpBinding.inflate(layoutInflater)
     }
     private val viewModel: OtpViewModel by viewModel { parametersOf(intent?.extras) }
+    private lateinit var sheetBinding: SheetRegistrationSuccessBinding
+    private lateinit var dialog: BottomSheetDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        sheetBinding = SheetRegistrationSuccessBinding.inflate(layoutInflater)
+        dialog = BottomSheetDialog(this, R.style.BottomSheetTheme)
+        dialog.setContentView(sheetBinding.root)
 
         setClickListeners()
         observeResult()
@@ -42,6 +56,7 @@ class OtpActivity : AppCompatActivity() {
                     binding.pbLoading.isVisible = false
                     binding.btnSubmit.isVisible = true
                     saveToken(it.payload)
+
                 },
                 doOnEmpty = {},
                 doOnError = {}
@@ -54,16 +69,31 @@ class OtpActivity : AppCompatActivity() {
         Log.d("token register", token.toString())
         if (!token.isNullOrBlank()) {
             token.let { SessionManager.saveAuthToken(this, it) }
-            navigateToHome()
+            bottomSheetSuccess()
         }
     }
 
+    private fun bottomSheetSuccess() {
+        dialog.show()
+        sheetBinding.btnHome.setOnClickListener {
+            navigateToHome()
+        }
+        sheetBinding.ibBack.setOnClickListener {
+            SessionManager.clearData(this)
+            navigateToRegister()
+        }
+    }
+
+    private fun navigateToRegister() {
+        startActivity(Intent(this, RegisterActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        })
+    }
+
     private fun navigateToHome() {
-        startActivity(
-            Intent(this, MainActivity::class.java).apply {
+        startActivity(Intent(this, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            }
-        )
+        })
     }
 
     private fun setClickListeners() {
