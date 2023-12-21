a5177ac23e0450a93d760e287db6ab19e6e11c23
Bxtohur <bitohur@gmail.com>
2023-12-21 22:42:19 +0700

    ganti tipe data dari course ke DetailCourseResponse, masih crash di bagian materi kelas

--- app/src/main/java/com/cious/learnhub/ui/detail/CourseDetailActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/detail/CourseDetailActivity.kt
@@ -182,7 +182,6 @@ class CourseDetailActivity : AppCompatActivity() {
                 youtubePlayer?.toggleFullscreen()
             }
         }
-
         super.onConfigurationChanged(newConfig)
     }
 
