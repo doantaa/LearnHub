16b666cf431756b61ec8ee962c10542af9198abb
doantaa <doantaaloycius@gmail.com>
2023-12-30 18:08:16 +0700

    ViewModel Typo

--- app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
+++ app/src/main/java/com/cious/learnhub/ui/main/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cious.learnhub.R
 import com.cious.learnhub.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val vieModel : MainViewModel by viewModel()
+    private val viewModel : MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private fun setupBottomNav() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.navView.setupWithNavController(navController)
-        if (!vieModel.isLogin){
+        if (!viewModel.isLogin){
             binding.navView.menu.findItem(R.id.navigation_my_class).isVisible = false
             binding.navView.menu.findItem(R.id.navigation_notifications).isVisible = false
         } else {
