ea1f6d0cb90a9079af53c395dc7bbf22d88c5ded
doantaa <doantaaloycius@gmail.com>
2024-01-04 16:00:55 +0700

    Unit Testing Profile Repository

--- app/src/test/java/com/cious/learnhub/data/repository/ProfileRepositoryImplTest.kt
+++ app/src/test/java/com/cious/learnhub/data/repository/ProfileRepositoryImplTest.kt
@@ -2,8 +2,12 @@ package com.cious.learnhub.data.repository
 
 import app.cash.turbine.test
 import com.cious.learnhub.data.network.api.datasource.ProfileDataSource
+import com.cious.learnhub.data.network.api.model.profile.ChangePasswordRequest
+import com.cious.learnhub.data.network.api.model.profile.ChangePasswordResponse
 import com.cious.learnhub.data.network.api.model.profile.ProfileRequest
 import com.cious.learnhub.data.network.api.model.profile.ProfileResponse
+import com.cious.learnhub.data.network.api.model.profile.UserTransactionItemResponse
+import com.cious.learnhub.data.network.api.model.profile.UserTransactionResponse
 import com.cious.learnhub.utils.ResultWrapper
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
@@ -80,6 +84,62 @@ class ProfileRepositoryImplTest {
             Assert.assertTrue(data is ResultWrapper.Loading)
             coVerify { dataSource.editData(any()) }
         }
+
+    }
+
+
+    @Test
+    fun `do change password, result success`() = runTest{
+        val mockRequest = mockk<ChangePasswordRequest>()
+        val mockResponse = mockk<ChangePasswordResponse>(relaxed = true)
+        coEvery { dataSource.changePassword(any()) } returns mockResponse
+
+        repository.doChangePassword(mockRequest).map {
+            delay(100)
+            it
+        }.test {
+            delay(201)
+            val data = expectMostRecentItem()
+            Assert.assertTrue(data is ResultWrapper.Success)
+            coVerify { dataSource.changePassword(any()) }
+        }
+
+    }
+
+
+
+    @Test
+    fun `get user transaction, result loading`() = runTest {
+        val mockResponse = mockk<UserTransactionResponse>(relaxed = true)
+        coEvery { dataSource.getUserTransaction() } returns mockResponse
+
+        repository.getUserTransaction().map {
+            delay(100)
+            it
+        }.test {
+            delay(101)
+            val data = expectMostRecentItem()
+            println(data)
+            Assert.assertTrue(data is ResultWrapper.Loading)
+            coVerify { dataSource.getUserTransaction() }
+        }
+    }
+        @Test
+        fun `get user transaction, result success`() = runTest{
+            val mockDetailResponse = mockk<UserTransactionItemResponse>(relaxed = true)
+            val mockResponse = UserTransactionResponse(data = listOf(mockDetailResponse), "message", true)
+            coEvery { dataSource.getUserTransaction() } returns mockResponse
+
+            repository.getUserTransaction().map {
+                delay(100)
+                it
+            }.test {
+                delay(201)
+                val data = expectMostRecentItem()
+                println(data)
+                Assert.assertTrue(data is ResultWrapper.Success)
+                coVerify { dataSource.getUserTransaction() }
+            }
 
     }
 
